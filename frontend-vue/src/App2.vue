4ee14438297dfe4b72888f8af24199bdb5bf704c
mousou85 <mousou85@gmail.com>
2022-02-16 17:30:48 +0900

    feat(frontend-vue): router 작업

---
+++ frontend-vue/src/App2.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Menu from './components/Menu.vue';
+</script>
+
+<template>
+  <header>
+    <Menu></Menu>
+  </header>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+header {
+  display: block;
+  border-bottom:1px solid;
+}
+</style>
---
+++ frontend-vue/src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="menuWrap">
+    <div class="item"></div>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<style>
+
+#menuWrap {
+  display: block;
+  margin: 0;
+  padding: 0;
+}
+
+#menuWrap .item {
+  display: inline-block;
+  border-right: 1px solid;
+  padding: 10px;
+}
+</style>
