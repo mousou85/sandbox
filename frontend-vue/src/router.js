1e13f90c9d2de83184ccd413192e2e7a702cf3e5
mousou85 <mousou85@gmail.com>
2022-04-15 13:04:18 +0900

    fix(frontend-vue): 라우터 수정

--- frontend-vue/src/router.js
+++ frontend-vue/src/router.js
@@ -3,28 +3,28 @@ import {createWebHistory, createRouter} from 'vue-router';
 const routes = [
   {
     path: '/',
-    component: import('./components/Index.vue')
+    component: () => import('./components/Index.vue')
   },
   {
     path: '/invest-history/',
     redirect: '/invest-history/history',
-    component: import('./components/investHistory/Index.vue'),
+    component: () => import('@/components/investHistory/Index.vue'),
     children: [
       {
         path: 'history',
-        component: import('./components/investHistory/History.vue')
+        component: () => import('@/components/investHistory/History.vue')
       },
       {
         path: 'company',
-        component: import('./components/investHistory/Company.vue')
+        component: () => import('@/components/investHistory/Company.vue')
       },
       {
         path: 'item',
-        component: import('./components/investHistory/Item.vue')
+        component: () => import('@/components/investHistory/Item.vue')
       },
       {
         path: 'unit',
-        component: import('./components/investHistory/Unit.vue')
+        component: () => import('@/components/investHistory/Unit.vue')
       },
     ]
   },
