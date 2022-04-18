46fa42b73cfdb979a58378feaf3febce450a599d
mousou85 <mousou85@gmail.com>
2022-04-18 09:06:22 +0900

    feat(backend-express): 재테크 히스토리 API 수정
    - 년간 summary api 추가

--- backend-express/routes/investHistory/summary.js
+++ backend-express/routes/investHistory/summary.js
@@ -191,6 +191,107 @@ module.exports = (db) => {
   }));
   
   /**
+   * summary year
+   */
+  router.get('/:item_idx([0-9]+)/year', asyncHandler(async (req, res) => {
+    try {
+      //set vars: request
+      const itemIdx = req.params.item_idx;
+      const unit = req.query.unit ? req.query.unit.toUpperCase() : 'KRW';
+      const year = req.query.year ?? dayjs().year();
+      
+      if (!itemIdx) throw new ResponseError('잘못된 접근');
+      
+      //item 유무 체크
+      const hasItem = await db.exists(db.queryBuilder()
+        .from('invest_item')
+        .where('item_idx', itemIdx)
+      );
+      if (!hasItem) throw new ResponseError('item이 존재하지 않음');
+      
+      //set vars: 요약 데이터
+      let query = db.queryBuilder()
+        .select()
+        .from('invest_summary_year AS s')
+        .join('invest_unit AS u', 's.unit_idx', 'u.unit_idx')
+        .where('s.item_idx', itemIdx)
+        .andWhere('s.summary_year', year)
+        .andWhere('u.unit', unit);
+      const rsSummary = await db.queryRow(query);
+      
+      // set vars: summary data
+      let summaryData = {
+        'year': year,
+        'unit': unit,
+        'unitType': '',
+        'inout': {
+          'total': 0,
+          'principalTotal': 0,
+          'principalPrev': 0,
+          'principalCurrent': 0,
+          'proceedsTotal': 0,
+          'proceedsPrev': 0,
+          'proceedsCurrent': 0,
+        },
+        'revenue': {
+          'total': 0,
+          'interestTotal': 0,
+          'interestPrev': 0,
+          'interestCurrent': 0,
+          'eval': 0,
+          'evalPrev': 0,
+        },
+        'earn': {
+          'earn': 0,
+          'rate': 0.0,
+          'earnIncProceeds': 0,
+          'rateIncProceeds': 0
+        },
+        'earnPrevDiff': {
+          'earn': 0,
+          'rate': 0.0,
+          'earnIncProceeds': 0,
+          'rateIncProceeds': 0
+        }
+      };
+      
+      
+      if (rsSummary) {
+        summaryData.unitType = rsSummary.unit_type;
+        
+        summaryData.inout.total = rsSummary.inout_total;
+        summaryData.inout.principalTotal = rsSummary.inout_principal_total;
+        summaryData.inout.principalPrev = rsSummary.inout_principal_prev;
+        summaryData.inout.principalCurrent = rsSummary.inout_principal_current;
+        summaryData.inout.proceedsTotal = rsSummary.inout_proceeds_total;
+        summaryData.inout.proceedsPrev = rsSummary.inout_proceeds_prev;
+        summaryData.inout.proceedsCurrent = rsSummary.inout_proceeds_current;
+        
+        summaryData.revenue.total = rsSummary.revenue_total;
+        summaryData.revenue.interestTotal = rsSummary.revenue_interest_total;
+        summaryData.revenue.interestPrev = rsSummary.revenue_interest_prev;
+        summaryData.revenue.interestCurrent = rsSummary.revenue_interest_current;
+        summaryData.revenue.eval = rsSummary.revenue_eval;
+        summaryData.revenue.evalPrev = rsSummary.revenue_eval_prev;
+        
+        summaryData.earn.earn = rsSummary.earn;
+        summaryData.earn.rate = rsSummary.earn_rate;
+        summaryData.earn.earnIncProceeds = rsSummary.earn_inc_proceeds;
+        summaryData.earn.rateIncProceeds = rsSummary.earn_rate_inc_proceeds;
+        
+        summaryData.earnPrevDiff.earn = rsSummary.earn_prev_diff;
+        summaryData.earnPrevDiff.rate = rsSummary.earn_rate_prev_diff;
+        summaryData.earnPrevDiff.earnIncProceeds = rsSummary.earn_inc_proceeds_prev_diff;
+        summaryData.earnPrevDiff.rateIncProceeds = rsSummary.earn_rate_inc_proceeds_prev_diff;
+      }
+      
+      res.json(createResult('success', summaryData));
+    } catch (err) {
+      throw err;
+    }
+  }));
+  
+  /**
    * summary remake
    */
   router.get('/remake-all', asyncHandler(async (req, res) => {
