ab493269889ade53b0252a8c2f69fdc540aac616
mousou85 <mousou85@gmail.com>
2022-04-19 13:47:54 +0900

    chore(frontend-vue): .env 적용

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -3,6 +3,11 @@ import { createApp } from 'vue'
 import App from '@/App2.vue';
 import router from '@/router';
 import store from '@/store/index';
+import {setAPIBaseUrl} from '@/modules/investHistory';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+setAPIBaseUrl(API_BASE_URL);
 
 const app = createApp(App);
 app.use(router);
