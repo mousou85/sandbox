b6112f2500e6c6c28a09e7b6ca6c927341e7c20d
mousou85 <mousou85@gmail.com>
2022-04-27 15:19:06 +0900

    feat(frontend-vue): ui framework 적용

--- frontend-vue/src/App2.vue
+++ frontend-vue/src/App2.vue
@@ -1,7 +1,3 @@
-<script setup>
-import Menu from './components/Menu.vue';
-</script>
-
 <template>
   <header class="block">
     <Menu></Menu>
@@ -10,6 +6,48 @@ import Menu from './components/Menu.vue';
     <router-view></router-view>
   </main>
 </template>
+
+<script>
+import {useStore} from "vuex";
+import {onMounted} from 'vue';
+
+import Menu from '@/components/Menu.vue';
+
+export default {
+  components: {
+    Menu
+  },
+  setup() {
+    //set vars: vuex
+    const store = useStore();
+
+    /**
+     * 모바일 여부 설정
+     */
+    const setMobileFlag = () => {
+      if (window.innerWidth >= 1024) {
+        store.dispatch('setMobile', false);
+      } else {
+        store.dispatch('setMobile', true);
+      }
+    }
+
+    onMounted(() => {
+      /*
+      load/resize 이벤트에 모바일 여부 설정 메소드 설정
+       */
+      window.addEventListener('load', () => {
+        setMobileFlag();
+      });
+
+      window.addEventListener('resize', () => {
+        setMobileFlag();
+      });
+    });
+  }
+}
+</script>
+
 
 <style scoped>
 </style>
--- frontend-vue/src/store/index.js
+++ frontend-vue/src/store/index.js
@@ -1,7 +1,42 @@
 import {createStore} from 'vuex';
-import {investHistory} from '@/store/modules/investHistory';
 
 export default createStore({
+  state: {
+    /**
+     * 모바일 여부
+     */
+    isMobile: false,
+  },
+  mutations: {
+    /**
+     * 모바일 여부 설정
+     * @param state
+     * @param {boolean} value
+     */
+    setMobile(state, value) {
+      state.isMobile = value;
+    }
+  },
+  getters: {
+    /**
+     * 모바일 여부 반환
+     * @param state
+     * @returns {boolean}
+     */
+    isMobile(state) {
+      return state.isMobile;
+    }
+  },
+  actions: {
+    /**
+     * 모바일 여부 설정
+     * @param commit
+     * @param {boolean} value
+     */
+    setMobile({commit}, value) {
+      commit('setMobile', value);
+    }
+  }
   // modules: {
   //   investHistory
   // }
