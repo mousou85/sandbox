e47180caf6cbbdf3468420af7090c2dbc74b725a
mousou85 <mousou85@gmail.com>
2022-08-24 16:27:13 +0900

    fix: env 설정 확인

--- frontend-vue/src/apis/base.js
+++ frontend-vue/src/apis/base.js
@@ -10,6 +10,7 @@ import * as _ from 'lodash';
  * @type {string}
  */
 const baseURL = import.meta?.env?.VITE_API_BASE_URL || 'http://localhost:5000';
+console.log(baseURL);
 
 export const apiBase = () => {
   //set vars: vuex, router
